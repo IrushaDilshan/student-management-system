b9b52916fa2dee52eb6d61a01ca8cebebe10c826
IrushaDilshan <it23768758@my.sliit.lk>
2026-07-21 19:31:36 +0530

    feat(repository): add JPA repositories for all entities

---
+++ backend/src/main/java/backend/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package backend.repository;
+
+import backend.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
